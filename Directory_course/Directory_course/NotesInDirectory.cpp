d0b29ef4704dde5a2f88044f203f1e93ba256766
Тимур Ефименко <yefimenkotimur@gmail.com>
2022-05-29 11:47:47 +0300

    Added: Phone GetPhone() const, string GetEmail() const implementation

--- Directory_course/Directory_course/NotesInDirectory.cpp
+++ Directory_course/Directory_course/NotesInDirectory.cpp
@@ -6,3 +6,14 @@ string NotesInDirectory::GetFirstEmail() const
 {
 	return e_mails.front();
 }
+
+Phone NotesInDirectory::GetPhone() const 
+{
+	return phone;
+}
+
+string NotesInDirectory::GetEmail() const 
+{
+	return email;
+}
+
