542db03bce03d38d4945b81649b84eb0ac768bd4
Тимур Ефименко <yefimenkotimur@gmail.com>
2022-05-29 11:36:33 +0300

    Added methods: void AddEmail, void AddPhone; std::string GetEmail, Phone GetPhone; fields: Phone phone, std::string email; new class Human: public NotesInDirectory

--- Directory_course/Directory_course/NotesInDirectory.cpp
+++ Directory_course/Directory_course/NotesInDirectory.cpp
@@ -1,1 +1,8 @@
 #include "NotesInDirectory.h"
+
+using namespace std;
+
+string NotesInDirectory::GetFirstEmail() const 
+{
+	return e_mails.front();
+}
--- Directory_course/Directory_course/NotesInDirectory.h
+++ Directory_course/Directory_course/NotesInDirectory.h
@@ -17,9 +17,19 @@ private:
 class NotesInDirectory
 {
 public:
+	void AddEmail(std::vector<std::string> e_mails, std::string email);
+	void Add(std::vector<Phone> phones, Phone phone);
+	std::string GetEmail() const;
+	Phone GetPhone() const;
 	std::string GetFirstEmail() const;
 private:
 	std::vector<Phone> phones;
 	std::vector<std::string> e_mails;
+	std::string email;
+	Phone phone;
 };
 
+class Human : public NotesInDirectory 
+{
+	
+};
